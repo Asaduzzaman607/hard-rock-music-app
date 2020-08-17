bcdd5bfa4c4e96cf29a03c5809b730ad48284a06
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-08-17 23:11:42 +0600

    final

--- script.js
+++ script.js
@@ -11,47 +11,48 @@ function getResults(value) {
 function displayResult(songs) {
     const allSongs = songs.data;
     const searchResult = document.getElementById("searchResult");
+    let htmlTemplate = '';
     for (let i = 0; i < 10; i++) {
         const title = allSongs[i].title;
         const artist = allSongs[i].artist.name;
         const album = allSongs[i].album.title;
         const albumCover = allSongs[i].album.cover;
-        searchResult.innerHTML += `<div class="single-result row my-3 p-3 d-flex justify-content-between align-items-center">
-                                    <div class="col-md-9">
-                                        <h3 id="title">${title}</h3>
-                                        <p class="author lead">Album by: <span>${album}</span></p>
-                                        <p>Artist: <span id="author">${artist}</span></p>
-                                    </div>
+        htmlTemplate += `<div class="single-result row my-3 p-3 d-flex justify-content-between align-items-center">
                                     <div>
                                        <img src="${albumCover}" alt="">
                                     </div>
-                                    <div class="col-md-3 text-md-right text-center">
-                                        <button class="getLyrics btn btn-success" onclick="getLyrics(this)">Get Lyrics</button>
+                                    <div class="col-md-6">
+                                        <h3 id="title">${title}</h3>
+                                        <p class="author lead">Album by: <span>${album}</span></p>
+                                        <p>Artist: <span id="author">${artist}</span></p>
+                                        <a href="${songs.data[i].preview}" class="btn btn-warning ml-3">play song</a>
                                     </div>
-                                </div>`
+                                    <div class="col-md-3 text-md-right text-center">
+                                        <button onclick="getLyrics('${artist}', '${title}')" class="btn btn-success" >Get Lyrics 
+                                        </button>
+                                    </div>
+                 </div>`;
     }
+    searchResult.innerHTML = htmlTemplate;
 }
 
 // get lyrics function
 
-function getLyrics(btn) {
-const getLyrics = document.getElementsByClassName("getLyrics");
-for (let i = 0; i < getLyrics.length; i++) {
-    if (btn == getLyrics[i]) {
-    const artist = document.getElementById("author").innerHTML;
-    const title = document.getElementById("title").innerHTML;
+function getLyrics(artist, title) {
     fetch(`https://api.lyrics.ovh/v1/${artist}/${title}`)
-    .then(response => response.json())
-    .then(function displayResult(lyric) {
-        const currentLyric = lyric.lyrics;
-        const displayArea = document.getElementById("lyricsArea");
-        displayArea.innerHTML = `<button class="btn go-back btn-success" onclick="hide()">Go-Back</button>
-                                <h2 class="text-success mb-4">${title}</h2>
-                                <pre class="lyric text-white">${currentLyric}</pre>`
+        .then(response => response.json())
+        .then(function displayLyric(data) {
+            const currentLyric = data.lyrics;
+            let lyric = '';
+            const displayArea = document.getElementById("lyricsArea");
+            lyric = `<button class="btn go-back btn-success" onclick="hide()">Go-Back</button>
+                            <h2 class="text-success mb-4">${title}</h2>
+                            <h4 class="text-success mb-4">${artist}</h4>
+                            <pre class="lyric text-white">${currentLyric}</pre>`
+            displayArea.innerHTML = lyric;
         })
-    .catch(error => console.log(error))
-    }
-}
+const displayArea = document.getElementById("lyricsArea");
+displayArea.style.display = "block";
 }
 
 //search area section
